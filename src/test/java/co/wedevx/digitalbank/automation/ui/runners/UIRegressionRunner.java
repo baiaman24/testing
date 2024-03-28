8522aa357d564d3e2df2e56e939538635371febf
Baiaman <baia0312@gmail.com>
2024-03-28 10:03:01 +0100

    commit to use jenkins

--- src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ui/features")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value = "co.wedevx.digitalbank.automation.ui.steps")
-@IncludeTags("newChecking")
+@ExcludeTags("@broken")
 public class UIRegressionRunner {
 
 
